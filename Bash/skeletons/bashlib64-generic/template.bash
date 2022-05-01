5fd8da9f393af7e97ca16f37981b17d3ec152896
serdigital64 <serdigital64@gmail.com>
2022-05-01 17:22:46 -0300

    feat: update catalog

--- Bash/skeletons/bashlib64-generic/template.bash
+++ Bash/skeletons/bashlib64-generic/template.bash
@@ -11,11 +11,23 @@
 source "${BASH_SOURCE[0]%/*}/bashlib64.bash" || { echo "Error: unable to load bashlib64" && exit 1; }
 # Alternative when the lib path is fixed # source "${X_PATH_TO_LIB_X}/bashlib64.bash" || exit 1
 
+#
+# Globals
+#
+# Template # Use this section to declare global variables only. Settings are done in the function X_APP_NAMESPACE_X_setup_globals
+
+export X_APP_NAMESPACE_X_X_EXPORT_RO_X
+export X_APP_NAMESPACE_X_X_EXPORT_X
+
+#
+# Functions
+#
+
 function X_APP_NAMESPACE_X_X_FUNCTION_COMMAND1_X() {
 
   local option="$1"
   local flag="$2"
-  local status=1
+  local -i status=1
 
   :
   status=$?
@@ -26,7 +38,7 @@ function X_APP_NAMESPACE_X_X_FUNCTION_COMMAND1_X() {
 
 function X_APP_NAMESPACE_X_X_FUNCTION_COMMAND2_X() {
 
-  local status=1
+  local -i status=1
 
   :
   status=$?
@@ -35,11 +47,19 @@ function X_APP_NAMESPACE_X_X_FUNCTION_COMMAND2_X() {
 
 }
 
-function X_APP_NAMESPACE_X_check() {
+# Template # Use this function to set global values only.
+function X_APP_NAMESPACE_X_setup_globals() {
+  readonly X_APP_NAMESPACE_X_X_EXPORT_RO_X=''
+  X_APP_NAMESPACE_X_X_EXPORT_X=''
+}
 
+# Template # Use this function for global requirements only.
+function X_APP_NAMESPACE_X_check_requirements() {
+
+  [[ -z "$X_APP_NAMESPACE_X_command" ]] && X_APP_NAMESPACE_X_help && exit 1
   #bl64_check_command '' || return 1
   #bl64_check_file '' || return 1
-  :
+  return 0
 
 }
 
@@ -64,16 +84,14 @@ function X_APP_NAMESPACE_X_help() {
 # Main
 #
 
-readonly X_APP_NAMESPACE_X_X_EXPORT_RO_X=''
-export X_APP_NAMESPACE_X_X_EXPORT_X=''
-
-declare X_APP_NAMESPACE_X_status=1
+declare -i X_APP_NAMESPACE_X_status=1
 declare X_APP_NAMESPACE_X_option=''
 declare X_APP_NAMESPACE_X_command=''
 declare X_APP_NAMESPACE_X_command_tag=''
 declare X_APP_NAMESPACE_X_X_OPTION_X=''
 declare X_APP_NAMESPACE_X_X_FLAG_X='0'
 
+X_APP_NAMESPACE_X_setup_globals
 (($# == 0)) && X_APP_NAMESPACE_X_help && exit 1
 while getopts ':xwy:zh' X_APP_NAMESPACE_X_option; do
   case "$X_APP_NAMESPACE_X_option" in
@@ -87,17 +105,16 @@ while getopts ':xwy:zh' X_APP_NAMESPACE_X_option; do
     ;;
   y) X_APP_NAMESPACE_X_X_OPTION_X="$OPTARG" ;;
   z) X_APP_NAMESPACE_X_X_FLAG_X='1' ;;
-  h) X_APP_NAMESPACE_X_help && exit ;;
-  \?) X_APP_NAMESPACE_X_help && exit 1 ;;
+  h | \? | *) X_APP_NAMESPACE_X_help && exit 1 ;;
   esac
 done
-[[ -z "$X_APP_NAMESPACE_X_command" ]] && X_APP_NAMESPACE_X_help && exit 1
-X_APP_NAMESPACE_X_check || exit 1
+X_APP_NAMESPACE_X_check_requirements || exit 1
 
 bl64_msg_show_batch_start "$X_APP_NAMESPACE_X_command_tag"
 case "$X_APP_NAMESPACE_X_command" in
 'X_APP_NAMESPACE_X_X_FUNCTION_COMMAND1_X') "$X_APP_NAMESPACE_X_command" "$X_APP_NAMESPACE_X_X_FLAG_X" "$X_APP_NAMESPACE_X_X_OPTION_X" ;;
 'X_APP_NAMESPACE_X_X_FUNCTION_COMMAND2_X') "$X_APP_NAMESPACE_X_command" ;;
+*) bl64_check_show_undefined "$X_APP_NAMESPACE_X_command" ;;
 esac
 X_APP_NAMESPACE_X_status=$?
 
