a140d45bc4cba6db138ade411f86862492d42a06
SerDigital64 (Max) <serdigital64@gmail.com>
2023-11-24 19:15:09 -0300

    feat: update catalog content (#16)

--- BashLib64/skeletons/script-single/main.bash
+++ BashLib64/skeletons/script-single/main.bash
@@ -6,7 +6,7 @@
 # Imports
 #
 
-# shellcheck source=SCRIPTDIR/bashlib64.bash
+# shellcheck source-path=SCRIPTDIR/X_PATH_TO_LIB_X
 source "${X_PATH_TO_LIB_X}/bashlib64.bash" || { echo "Error: unable to load bashlib64" && exit 1; }
 
 # X_IMPORTS_PLACEHOLDER_X
